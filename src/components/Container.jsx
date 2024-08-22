ba79d4aa4f90b668e2eb30fa6f20953185acdf8b
prabhjotaulakh123 <prabhjotaulakh68@gmail.com>
2024-08-22 19:48:24 -0400

    added navbar and homepage

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,8 +2,8 @@ import './Container.css'
 
 const Container = ({children}) => {
     return (
-        <main>
-            {children}
+        <main className='container-fluid px-3 px-md-5 px-lg-10'>
+            <div className='__container'>{children}</div>
         </main>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,27 @@
+import './Navbar.css'
+import { Link } from "react-router-dom"
+
 const Navbar = () => {
     return (
-        <nav>navbar</nav>
+        <nav className='navbar navbar-expand-lg navbar-dark bg-dark px-3 d-flex justify-content-between'>
+            <Link to='/' className='navbar-brand'>Fuel Logging</Link>
+            <div className='d-none d-lg-flex'>
+                <ul className='navbar-nav'>
+                    <li className='nav-item active'>
+                        <Link to='/' className='nav-link'>Register</Link>
+                    </li>
+                    <li className='nav-item active'>
+                        <Link to='/' className='nav-link'>Login</Link>
+                    </li>
+                    <li className='nav-item active'>
+                        <a href='https://github.com/prabhjotaulakh159/fuel-logging-api-client' className='nav-link'>GitHub</a>
+                    </li>
+                </ul>
+            </div>
+            <div className='d-block d-md-none'>
+                <i className='__menu bi bi-list'></i>
+            </div>
+        </nav>
     )
 }
 
